6dc1f4cfb36cd209e159b7612b11760af7991030
achmadlakisatya-rgb <achmadlakisatya@gmail.com>
2026-07-28 12:14:17 +0700

    Add files via upload

---
+++ objek2.js
@@ -0,0 +1,16 @@
+const karakter = (
+      nama: "Satya"
+      
+)
+//Dot Notation
+console.log(karakter.nama);
+console.log(karakter.level);
+console.log(karakter.HP);
+console.log(karakter.senjata);
+//Bracket Notation
+console.log(karakter['nama']);
+console.log(karakter['level']);
+console.log(karakter['HP']);
+console.log(karakter['senjata']);
+//template literal
+console.log('Selamat datang $[karakter.nama]'), levelmu $(karakter.level), HP-mu $(karakter.HP),
